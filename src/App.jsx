822c2d36c1455bfe255782918eec1165fd38fd0d
Michael Muga <mchlmuga@gmail.com>
2026-05-21 15:36:59 +0300

    1st comimit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,44 @@
+
+import { lazy, Suspense } from "react";
+import LazyLoad from "react-lazyload";
+
+const Hero = lazy(() => import("./components/hero/Hero"));
+const Portfolio = lazy(() => import("./components/portfolio/Portfolio"));
+const Contact = lazy(() => import("./components/contact/Contact"));
 const App = () => {
   return (
-    <div className=''>Hello World</div>
-  )
-}
+    <div className="container">
+        
+          <Suspense fallback={"loading..."}>
+        <LazyLoad height={"100vh"} offset={-100}>
+          <section id="#home">
+            <Hero />
+          </section>
+        </LazyLoad>
+      </Suspense>
+       <Suspense fallback={"loading..."}>
+        <LazyLoad height={"600vh"} offset={-100}>
+          {/* <section id="#portfolio"> */}
+          <Portfolio />
+          {/* </section> */}{" "}
+        </LazyLoad>
+      </Suspense>
 
-export default App
+      <Suspense fallback={"loading..."}>
+        <LazyLoad height={"100vh"} offset={-100}>
+          <section id="#contact">
+            <Contact />
+          </section>{" "}
+        </LazyLoad>
+      </Suspense>
+        
+
+
+        
+          
+    
+    </div>
+  );
+};
+
+export default App;
